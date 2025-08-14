8661a40e01194a6ec74e1aaf725a251dded7c81f
Tipriest <a1503741059@163.com>
2025-08-14 14:27:18 +0800

    add msg docs.h

---
+++ include/main.hpp
@@ -0,0 +1,68 @@
+#pragma once
+#include "joystick.h"
+#include "msg_docs.h"
+#include <sensor_msgs/Joy.h>
+#include <sensor_msgs/JoyFeedback.h>
+#include <sensor_msgs/JoyFeedbackArray.h>
+#include <string>
+
+// values
+int max_value_ = (1 << 15); // 16 bits joystick
+Joystick *js_;
+JoystickId js_id_;
+
+// 初始化手柄，设置按键和轴的映射关系
+void joystick_setup(std::string device, std::string js_type, int bits) {
+  js_ = new Joystick(device);
+  if (!js_->isFound()) {
+    std::cout << "Error: Joystick open failed." << std::endl;
+    exit(1);
+  }
+
+  max_value_ = (1 << (bits - 1));
+
+  // Xbox 手柄映射
+  if (js_type == "xbox") {
+    std::cout << "Joystick Type is xbox" << std::endl;
+    js_id_.axis["LX"] = 0; // 左摇杆X
+    js_id_.axis["LY"] = 1; // 左摇杆Y
+    js_id_.axis["RX"] = 3; // 右摇杆X
+    js_id_.axis["RY"] = 4; // 右摇杆Y
+    js_id_.axis["LT"] = 2; // 左扳机
+    js_id_.axis["RT"] = 5; // 右扳机
+    js_id_.axis["DX"] = 6; // 十字键X
+    js_id_.axis["DY"] = 7; // 十字键Y
+
+    js_id_.button["X"] = 2;
+    js_id_.button["Y"] = 3;
+    js_id_.button["B"] = 1;
+    js_id_.button["A"] = 0;
+    js_id_.button["LB"] = 4;
+    js_id_.button["RB"] = 5;
+    js_id_.button["SELECT"] = 6;
+    js_id_.button["START"] = 7;
+  }
+  // Switch 手柄映射
+  else if (js_type == "switch") {
+    std::cout << "Joystick Type is xbox" << std::endl;
+    js_id_.axis["LX"] = 0; // 左摇杆X
+    js_id_.axis["LY"] = 1; // 左摇杆Y
+    js_id_.axis["RX"] = 3; // 右摇杆X
+    js_id_.axis["RY"] = 4; // 右摇杆Y
+    js_id_.axis["LT"] = 2; // 左扳机
+    js_id_.axis["RT"] = 5; // 右扳机
+    js_id_.axis["DX"] = 6; // 十字键X
+    js_id_.axis["DY"] = 7; // 十字键Y
+
+    js_id_.button["X"] = 2;
+    js_id_.button["Y"] = 3;
+    js_id_.button["B"] = 1;
+    js_id_.button["A"] = 0;
+    js_id_.button["LB"] = 4;
+    js_id_.button["RB"] = 5;
+    js_id_.button["SELECT"] = 6;
+    js_id_.button["START"] = 7;
+  } else {
+    std::cout << "current not supported gamepad." << std::endl;
+  }
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,13 @@
-#include "joystick.h"
+#include "main.hpp"
+
+
 #include <iostream>
 #include <map>
-#include <string>
+
 using namespace std;
 
-int max_value_ = (1 << 15); // 16 bits joystick
-Joystick *js_;
-JoystickId js_id_;
+
+
 
 struct SimulationConfig {
   int use_joystick = 1;
@@ -15,61 +16,7 @@ struct SimulationConfig {
   int joystick_bits = 16;
 } config;
 
-// 初始化手柄，设置按键和轴的映射关系
-void joystick_setup(std::string device, std::string js_type, int bits) {
-  js_ = new Joystick(device);
-  if (!js_->isFound()) {
-    cout << "Error: Joystick open failed." << endl;
-    exit(1);
-  }
 
-  max_value_ = (1 << (bits - 1));
-
-  // Xbox 手柄映射
-  if (js_type == "xbox") {
-    cout << "Joystick Type is xbox" << endl;
-    js_id_.axis["LX"] = 0; // 左摇杆X
-    js_id_.axis["LY"] = 1; // 左摇杆Y
-    js_id_.axis["RX"] = 3; // 右摇杆X
-    js_id_.axis["RY"] = 4; // 右摇杆Y
-    js_id_.axis["LT"] = 2; // 左扳机
-    js_id_.axis["RT"] = 5; // 右扳机
-    js_id_.axis["DX"] = 6; // 十字键X
-    js_id_.axis["DY"] = 7; // 十字键Y
-
-    js_id_.button["X"] = 2;
-    js_id_.button["Y"] = 3;
-    js_id_.button["B"] = 1;
-    js_id_.button["A"] = 0;
-    js_id_.button["LB"] = 4;
-    js_id_.button["RB"] = 5;
-    js_id_.button["SELECT"] = 6;
-    js_id_.button["START"] = 7;
-  }
-  // Switch 手柄映射
-  else if (js_type == "switch") {
-    cout << "Joystick Type is xbox" << endl;
-    js_id_.axis["LX"] = 0; // 左摇杆X
-    js_id_.axis["LY"] = 1; // 左摇杆Y
-    js_id_.axis["RX"] = 3; // 右摇杆X
-    js_id_.axis["RY"] = 4; // 右摇杆Y
-    js_id_.axis["LT"] = 2; // 左扳机
-    js_id_.axis["RT"] = 5; // 右扳机
-    js_id_.axis["DX"] = 6; // 十字键X
-    js_id_.axis["DY"] = 7; // 十字键Y
-
-    js_id_.button["X"] = 2;
-    js_id_.button["Y"] = 3;
-    js_id_.button["B"] = 1;
-    js_id_.button["A"] = 0;
-    js_id_.button["LB"] = 4;
-    js_id_.button["RB"] = 5;
-    js_id_.button["SELECT"] = 6;
-    js_id_.button["START"] = 7;
-  } else {
-    cout << "current not supported gamepad." << endl;
-  }
-}
 typedef union {
   struct {
     uint8_t R1 : 1;
@@ -118,8 +65,8 @@ std::map<string, int> ButtonId = {
 int main(int argc, char **argv) {
   joystick_setup(config.joystick_device, config.joystick_type,
                  config.joystick_bits);
-  while (false) {
-    usleep(1000);
+  while (true) {
+    usleep(10000);
     JoystickEvent jsevent;
     if (js_->sample(&jsevent)) {
       if (jsevent.isButton()) {
@@ -131,7 +78,7 @@ int main(int argc, char **argv) {
     }
   }
 
-  while (true) {
+  while (false) {
     js_->getState();
 
     unitree_key.components.R1 = js_->button_[ButtonId["RB"]];
